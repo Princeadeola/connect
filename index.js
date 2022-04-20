1f52aa762ef76664012ca891ce9c278e442d5a62
Adesipe Adeola <56828114+Princeadeola@users.noreply.github.com>
2022-04-20 13:25:24 -0700

    commited

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 
 //  SIDE BAR
@@ -91,11 +96,24 @@ const closeThemeModal = (e) => {
 themeModal.addEventListener('click', closeThemeModal);
 
 
+
 // fonts
+
+// remove active class from span or font size selectors
+const removeSizeSelector = () => {
+	fontSizes.forEach(size => {
+		size.classList.remove('active');
+	})
+}
+
+
 fontSizes.forEach(size => {
-	let fontSize;
 
 	size.addEventListener('click', () => {
+		removeSizeSelector();	
+		let fontSize;
+		size.classList.toggle('active');
+
 		if (size.classList.contains('font-size-1')) {
 			fontSize = '10px';
 			// root.style.setProperty('')
@@ -124,3 +142,83 @@ fontSizes.forEach(size => {
 
 	})
 })
+
+
+// remove active class from colors
+const changeActiveColorClass = () => {
+	colorPalette.forEach(colorPicker => {
+		colorPicker.classList.remove('active');
+	})
+}
+
+// change primary colors
+colorPalette.forEach(color => {
+	color.addEventListener('click', () => {
+		let primary;
+		changeActiveColorClass();
+
+		if (color.classList.contains('color-1')) {
+			primaryHue = 252;
+		}else if (color.classList.contains('color-2')) {
+			primaryHue = 52;
+		}else if (color.classList.contains('color-3')) {
+			primaryHue = 352;
+		}else if (color.classList.contains('color-4')) {
+			primaryHue = 152;
+		}else if (color.classList.contains('color-5')) {
+			primaryHue = 202;
+		}
+		color.classList.add('active');
+
+		root.style.setProperty('--primary-color-hue', primaryHue);
+	})
+})
+
+// Background values
+let darkColorLightness;
+let lightColorLightness;
+let whiteColorLightness;
+
+// change background color
+const changeBG = () => {
+	root.style.setProperty('--light-color-lightness', lightColorLightness);
+	root.style.setProperty('--white-color-lightness', whiteColorLightness);
+	root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', () => {
+	//add active class
+	Bg1.classList.add('active');
+	//remove active class from the others
+	Bg2.classList.remove('active');
+	Bg3.classList.remove('active');
+	// remove customized changes from local storage
+	window.location.reload();
+})
+
+Bg2.addEventListener('click', () => {
+	darkColorLightness = '95%';
+	whiteColorLightness = '20%';
+	lightColorLightness = '15%';
+
+	//add active class
+	Bg2.classList.add('active');
+	//remove active class from the others
+	Bg1.classList.remove('active');
+	Bg3.classList.remove('active');
+	changeBG();
+})
+
+Bg3.addEventListener('click', () => {
+	darkColorLightness = '95%';
+	whiteColorLightness = '10%';
+	lightColorLightness = '10%';
+
+	//add active class
+	Bg3.classList.add('active');
+	//remove active class from the others
+	Bg1.classList.remove('active');
+	Bg2.classList.remove('active');
+	changeBG();
+})
+
